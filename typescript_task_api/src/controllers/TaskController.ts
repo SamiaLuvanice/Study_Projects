2302f72c9cc0b85355986a951fc5817c62c7e0fa
Sâmia Luvanice <samia.luvanice.dev@gmail.com>
2026-01-24 15:37:46 -0300

    feat: finished tasks api

---
+++ typescript_task_api/src/controllers/TaskController.ts
@@ -0,0 +1,59 @@
+import type { Handler, Request, Response } from "express";
+import { z } from "zod";
+import { HttpError } from "../errors/HttpError.js";
+import { Task } from "../models/Task.js";
+
+const StoreRequestSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  status: z.enum(["todo", "doing", "done"]),
+  priority: z.enum(["low", "medium", "high"]),
+});
+
+const UpdateRequestSchema = z.object({
+  title: z.string().optional(),
+  description: z.string().optional(),
+  status: z.enum(["todo", "doing", "done"]).optional(),
+  priority: z.enum(["low", "medium", "high"]).optional(),
+});
+
+export class TaskController {
+  // GET /api/tasks
+    index: Handler = (req, res) => {
+      const tasks = Task.findAll();
+      return res.json(tasks);
+    }
+
+    // POST /api/tasks
+    store = (req: Request, res:  Response) => {
+      const parsedBody = StoreRequestSchema.parse(req.body); // Validate request body
+      const newTask = Task.create(parsedBody);
+      res.status(201).json(newTask);
+    }
+
+    // GET /api/tasks/:id
+    show: Handler = (req, res) => {
+      const { id } = req.params;
+      const task = Task.findById(Number(id));
+      if (!task) throw new HttpError(404, "Task not found");
+      res.json(task);
+    }
+
+    // PUT /api/tasks/:id
+    update: Handler = (req, res) => {
+      const { id } = req.params;
+      const parsedBody = UpdateRequestSchema.parse(req.body); // Validate request body
+      const updatedTask = Task.update(Number(id), parsedBody);
+      if (!updatedTask) throw new HttpError(404, "Task not found");
+      res.json(updatedTask);
+    }
+
+    // DELETE /api/tasks/:id
+    delete: Handler = (req, res) => {
+      const { id } = req.params;
+      const deletedTask = Task.delete(Number(id));
+      if (!deletedTask) throw new HttpError(404, "Task not found");
+      res.status(204).send();
+    }
+
+}
--- typescript_task_api/src/server.ts
+++ typescript_task_api/src/server.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import { errorHandler } from './middlewares/error-handler.js';
+import { router } from './routes.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api", router);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado em http://localhost:${PORT}`);
