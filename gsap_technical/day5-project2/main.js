0fb037729ed136ab2189590ef52ed589129d1ae0
Sâmia Luvanice <samia.luvanice.dev@gmail.com>
2026-04-30 16:03:43 -0300

    feat(day5): continue gsap tools of the project2

--- gsap_technical/day5-project2/main.js
+++ gsap_technical/day5-project2/main.js
@@ -1,21 +1,59 @@
-// Control Properties
-// duracion, delay, repeat, yoyo, ease, etc
+// duration 
+// delay
+// repeat
+// yoyo 
 
-gsap.to(".ball", {
-  y: -250,
-  duration: 1,
-  // delay: 2,
-  repeat: -1,
-  yoyo: true,
-  ease: "power1.inOut" 
+
+gsap.from(".logo", {
+    y: -100,
+    duration: 1
 })
 
-gsap.to(".shadow", {
-  scaleX: 0.5,
-  opacity: 0.3,
-  duration: 1,
-  repeat: -1,
-  yoyo: true,
-  delay: 2,
-  ease: "power1.inOut",
+gsap.from(".menu", {
+    y: -100,
+    duration: 1,
+    delay: 1
+})
+
+
+gsap.from(".btn", {
+    duration: 1,
+    scale: 0.9,
+    repeat: -1,
+    yoyo: true
+})
+
+
+
+gsap.from("#page h6", {
+    y: "-200%",
+    duration: 2,
+    delay: 2
+})
+
+
+gsap.from("#page h1", {
+    y: "-200%",
+    duration: 3,
+    delay: 3
+})
+
+
+gsap.from("#page .image img", {
+    y: "200%",
+    duration: 3,
+    delay: 3
+})
+
+gsap.from("#page .image .btn-group", {
+    y: "200%",
+    duration: 3,
+    delay: 3
+})
+
+
+gsap.from("#page .left-side", {
+    x: "-120%",
+    duration: 3,
+    delay: 3
 })
